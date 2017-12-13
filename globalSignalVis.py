d59e2d7e75fed46038ba50bb413d8bf9d7c70e53
Kara <karakundert@gmail.com>
2017-12-13 10:18:56 -0800

    corrected rotations in globalSkyModel to actually be correct, added time + sky rotations to globalSignalVis, modified calcVis plotting function to show current topocentric sky (without beam, to be brought back in the future), switched to brightness temperature vs. jansky, added global sky model map functionality

--- globalSignalVis.py
+++ globalSignalVis.py
@@ -1,112 +1,86 @@
 #! /usr/bin/env python
 import aipy as a
-#import capo
 import uvtools
 import numpy as np
 import os
 import hypersim.absorber as ab
 import matplotlib.pylab as pl
-#from GlobalSkyModel import GlobalSkyModel
+import sys
+sys.path.append('/home/kara/capo/kmk/scripts')
+from GlobalSkyModel import GlobalSkyModel
 
-def makeFlatMap(nside, freq, Tsky=1.0, jansky = True):
+def makeFlatMap(nside, freq, Tsky=1.0):
     """ fill sky map of given size and frequency with flat temperature across whole sky,
         returns map in Janskys. """
     hpm = a.healpix.HealpixMap(nside=nside)
     hpm.map = Tsky*np.ones(shape = hpm.map.shape)
     print "flat map size = " + str(hpm.map.shape)
-    if jansky == True:
-        hpm = hpm_TtoJy(hpm, freq)
     return hpm
 
-def makeSynchMap(nside, freq=0.150, jansky = True):
+def makeSynchMap(nside, freq=0.150):
     """ fill sky map of given size and frequency with flat temperature across whole sky based on synchrotron spectrum,
         returns map in Janskys. """
     hpm = a.healpix.HealpixMap(nside=nside)
     Tsky = 237 * (freq/0.150)**-2.5
     hpm.map = Tsky*np.ones(shape = hpm.map.shape)
     print "synchrotron map size = " + str(hpm.map.shape)
-    if jansky == True:
-        hpm = hpm_TtoJy(hpm, freq)
     return hpm
 
-def makePointSourceMap(nside, freq=0.150, Tsource=1., jansky = True):
+def makePointSourceMap(nside, xyz, freq=0.150, Tsource=1.):
     """ make sky map with point source with brightness temperature Tsource at zenith, for testing purposes"""
     hpm = a.healpix.HealpixMap(nside=nside)
-    xyz = hpm.px2crd(np.arange(hpm.npix()))
-    theta = np.arcsin(xyz[2])
-    hpm.map = np.where(np.sin(theta) == 0.0, Tsource, 0)
+    x,y,z = xyz
+    hpm[x,y,z] = Tsource
     print "point source map size = " + str(hpm.map.shape)
-    if jansky == True:
-        hpm = hpm_TtoJy(hpm, freq)
     return hpm
 
-def hpm_TtoJy(hpm, freq):
-    """ converts given Healpix map from brightness temperature to Janskys, provided map
-        frequency """
-    wvlen = a.const.c / (freq*1e9)
-    hpm.map *= 1e23*(4*np.pi/hpm.npix())*2*a.const.k/wvlen**2 # convert to Jy to make summable
-    return hpm
+def makeGSM(nside, freq, path='/home/kara/capo/kmk/gsm/gsm_raw/'):
+    """ make sky map with Global Sky Model, in Kelvin"""
+    # I'm p sure GlobalSkyModel returns maps in brightness temp from the source 
+    # code, but not 100% sure??
+    gsmMap = a.healpix.HealpixMap(nside=nside)
+    filename = "gsm_%dMHz.fits" % int(1000*freq)
+    if os.path.isfile(filename)==True:
+        gsmMap.from_fits(filename)
+    else:
+        g = a.healpix.HealpixMap(nside=512)
+        g.map = GlobalSkyModel(freq=1000*freq, GSMlocation=path, GSMNSIDE=512).m
+        gsmMap.from_hpm(g)
+        gsmMap.to_fits(filename)
+    return gsmMap
 
-#def makeGSM(path, nside, freq):
-#        gsmMap = a.healpix.HealpixMap(nside=nside)
-#        g = a.healpix.HealpixMap(nside=512)
-#        g.map = GlobalSkyModel(freq=1000*freq, GSMlocation=path, 
-#        GSMNSIDE=512).map # galactic
-#        g = hpm_TtoJy(g, freq=freq)
-#        gsmMap.from_hpm(g)
-#        return gsmMap
-
-#def makeGSMMap(array, nside, filename, freq, path='/home/kara/capo/kmk/gsm/gsm_raw/'):
-#    """ create a Healpix map of a given size filled with a simulated global 
-#    sky model
-#        at a given frequency """
-#    makeGSM(path=path, filename=filename, sfreq=freq, sdf=10, num=1)
-#    g = a.healpix.HealpixMap(nside=512)
-#    gsm = a.healpix.HealpixMap(nside=nside)
-#    print "GSM map size = " + str(gsm.map.shape)
-#    d = np.loadtxt(path + filename + str(1001) + '.dat')
-#    g.map = d
-#    g = hpm_TtoJy(g, freq)
-#    gsm.from_hpm(g) # hack to lower resolution to prevent memory overload
-#    # convert to topocentric coordinates
-#    i, j, k = ijk = makeTop(gsm) #topocentric
-#    return gsm
-
-def makeTop(hpm):
-    # returns topocentric coordinates for a given healpix map
-    gxyz = gx,gy,gz, = hpm.px2crd(np.arange(hpm.npix())) # galactic
-    ga2eq = a.coord.convert_m(isys='eq', osys='ga', oepoch=aa.epoch) 
-    # conversion matrix so the isys and osys are reversed
-    exyz = ex,ey,ez = np.dot(ga2eq, hpm.px2crd(np.arange(hpm.npix()))) # equatorial
-    txyz = tx,ty,tz = np.dot(a.coord.eq2top_m(aa.sidereal_time(), aa.lat), exyz) # topocentric
-    return txyz
-
-def calcVis(aa, sky, nside, bl, freq, smooth, theta_cutoff, abs_file, flat = False, make_plot = True):
-    # TODO: import array + GSMMap, calculate topocentric coordinates on the 
-    # fly, generate PB on the fly, include time
+def calcVis(aa, sky, txyz, nside, bl, freq, smooth, theta_cutoff, abs_file, flat = False, make_plot = True):
     """ simulate sky visibilities for a given baseline and primary beam, 
         provided with a sky map at a known frequency and its coordinate system """
-    tx,ty,tz= txyz = sky.px2crd(np.arange(sky.npix()))
+    tx,ty,tz = txyz
     bxyz = aa.get_baseline(*bl, src='z')
     # generate proper PB
     abs = ab.BeamAbsorber(freqs=freq, horizon_angle=theta_cutoff)
     beam = np.abs(abs.response(txyz, smooth=smooth, data_file = abs_file, flat = False, use_abs=True))**2
     # attenuate sky signal and visibility by primary beam
-    obs_sky = a.healpix.HealpixMap(nside=nside)
-    obs_sky.map = beam[0] * sky.map
+    obs_sky = beam[0] * sky.map
+    print obs_sky.max()
     if make_plot == True:
-        uvtools.plot.plot_hmap_ortho(obs_sky, res=1, mx=2.5, drng=2.5)
+        pl_map = a.healpix.HealpixMap(nside=nside, interp=True)
+        x,y,z = xyz = pl_map.px2crd(np.arange(pl_map.npix()))
+        top2eq = np.linalg.inv(aa.eq2top_m)
+        ex,ey,ez = np.dot(top2eq, xyz)
+        # no beam in plot because I can't figure out how to make it work lmao
+        pl_map.map = sky[ex,ey,ez] 
+        # disappearing pixel at zenith even with interpolation on
+        uvtools.plot.plot_hmap_ortho(pl_map, res=1, mx=2.5, drng=2.5)
         pl.colorbar()
         pl.show()
     phs = np.exp(np.complex128(-2j*np.pi*freq*np.dot(bxyz, txyz)))
-    vis = np.sum(np.where(tz>0, obs_sky.map*phs, 0))
+    vis = (4*np.pi/sky.npix())*np.sum(np.where(tz>0, obs_sky*phs, 0))
     return vis
 
 def monopole_coeffs(aa, nside, bl, freq):
     # does this need to have absorber abilities?
     # I bet it does
     I_sky = makeFlatMap(nside=nside, freq=freq, Tsky=1.)
-    coefficient = calcVis(aa=aa, sky=I_sky, nside=nside, bl=bl, freq=freq, smooth=0.01, theta_cutoff=np.pi/4, abs_file = False, flat=0., make_plot=False)
+    txyz = I_sky.px2crd(np.arange(I_sky.npix()))
+    coefficient = calcVis(aa=aa, sky=I_sky, txyz = txyz, nside=nside, bl=bl, freq=freq, smooth=0.01, theta_cutoff=np.pi/4, abs_file = False, flat=0., make_plot=False)
     return coefficient
 
 def noise(shape, scale):
@@ -123,7 +97,7 @@ if __name__ == '__main__':
     o.add_option('--absfile', default='DIP_S11_LIDON_FER_DB.csv')
     o.add_option('--sim_dir', default='/home/kara/capo/kmk/scripts/')
     o.add_option('--smooth', default=0.1)
-    #o.add_option('--gsm_dir', default='/home/kara/capo/kmk/gsm/gsm_raw/')
+    o.add_option('--gsm_dir', default='/home/kara/capo/kmk/gsm/gsm_raw/')
 
     #o.add_option('--fileout', default='sim_results.uv')
 
@@ -132,7 +106,7 @@ if __name__ == '__main__':
     absfile = opts.absfile
     sim_dir = opts.sim_dir
     smooth = float(opts.smooth)
-    #gsm_dir = opts.gsm_dir
+    gsm_dir = opts.gsm_dir
 
     #fileout = opts.fileout
 
@@ -141,6 +115,8 @@ if __name__ == '__main__':
     wvlens = a.const.c / (freqs*1e9) # cm
         
     aa = a.cal.get_aa(calfile, freqs)
+    jd_init = 2458080
+    jds = np.linspace(jd_init, jd_init + 0.15, 4)
 
     N = 32
 
@@ -150,7 +126,8 @@ if __name__ == '__main__':
     B = 1e9*(np.max(freqs)-np.min(freqs))/len(freqs) # channel bandwidth in Hz
     int_time = 10 # integration time in seconds
     T_sys = 300 # K, room temperature system
-    T_rms = T_sys / np.sqrt(B*int_time)
+    #T_rms = T_sys / np.sqrt(B*int_time)
+    T_rms = 0.
 
     sim_data = []
     vis_data = np.zeros(len(freqs), dtype=complex)
@@ -158,20 +135,31 @@ if __name__ == '__main__':
     temp_wgt = []
     temp = []
     pl.figure(0)
+    
+    I_sky_eq = a.healpix.HealpixMap(nside=N)
     for i in xrange(len(ij)):
         bl = (0, ij[i])
         for j in xrange(len(freqs)):
-            I_sky = makeFlatMap(nside=N, freq=freqs[j], Tsky=1., jansky = True)
-            #I_sky = makeSynchMap(nside=N, freq=freqs[j], jansky = True)
-            cal_coeffs = monopole_coeffs(aa=aa, nside=N, bl=bl, freq=freqs[j])
-            obs_vis = calcVis(aa=aa, sky=I_sky, nside=N, bl=bl, freq=freqs[j], smooth=smooth, theta_cutoff=np.pi/4, abs_file = False, flat=0., make_plot=False)
-            noise_amp = 1e23 * 2*a.const.k*T_rms / (wvlens[j]**2) # convert thermal noise level to Janskys
-            obs_vis += noise(shape=obs_vis.size, scale=noise_amp)
-            #obs_vis = calcVis(aa=aa, sky=I_sky, nside=N, bl=bl, freq=freqs[j], smooth=smooth, theta_cutoff=np.pi/4, abs_file = absfile, flat=0, make_plot=False)
-            weight = (np.abs(cal_coeffs) / noise_amp)**2
-            temp_sum.append(weight * obs_vis / cal_coeffs)
-            temp_wgt.append(weight)
-            #temp.append(obs_vis/cal_coeffs)
+            # make sky with point source at zenith in equatorial coordinates
+            aa.set_jultime(jds[0])
+            flatMap = makeFlatMap(nside=N, freq=freqs[j], Tsky=1.)
+            point_source_loc = np.array([np.cos(aa.lat)*np.cos(aa.sidereal_time()), np.cos(aa.lat)*np.sin(aa.sidereal_time()), np.sin(aa.lat)])
+            pointMap = makePointSourceMap(nside=N, xyz=point_source_loc, freq = freqs[j], Tsource=100.)
+            I_sky_eq.map = flatMap.map + pointMap.map
+            exyz = ex,ey,ez = I_sky_eq.px2crd(np.arange(I_sky_eq.npix())) # topocentric
+            for k in xrange(len(jds)):
+                # convert maps from equatorial to topocentric coordinates
+                aa.set_jultime(jds[k])
+                eq2top = aa.eq2top_m
+                txyz = tx,ty,tz = np.dot(eq2top, exyz)
+                cal_coeffs = monopole_coeffs(aa=aa, nside=N, bl=bl, freq=freqs[j])
+                obs_vis = calcVis(aa=aa, sky=I_sky_eq, txyz = txyz, nside=N, bl=bl, freq=freqs[j], smooth=smooth, theta_cutoff=np.pi/4, abs_file = False, flat=0., make_plot=True)
+                noise_amp = 1e23 * 2*a.const.k*T_rms / (wvlens[j]**2) # convert thermal noise level to Janskys
+                #obs_vis += noise(shape=obs_vis.size, scale=noise_amp)
+                #obs_vis = calcVis(aa=aa, sky=I_sky, nside=N, bl=bl, freq=freqs[j], smooth=smooth, theta_cutoff=np.pi/4, abs_file = absfile, flat=0, make_plot=False)
+                weight = (np.abs(cal_coeffs) / noise_amp)**2
+                temp_sum.append(weight * obs_vis / cal_coeffs)
+                temp_wgt.append(weight)
             vis_data[j] = obs_vis
         bx,by,bz = bxyz = aa.get_baseline(*bl, src='z')
         uv = np.sqrt(bx**2 + by**2 + bz**2) / wvlens
--- globalSkyModelRot.py
+++ globalSkyModelRot.py
@@ -10,11 +10,16 @@ sys.path.append('/home/kara/capo/kmk/scripts')
 from GlobalSkyModel import GlobalSkyModel
 
 def makeGSM(nside, freq, path='/home/kara/capo/kmk/gsm/gsm_raw/'):
-        gsmMap = a.healpix.HealpixMap(nside=nside)
+    gsmMap = a.healpix.HealpixMap(nside=nside)
+    filename = "gsm_%dMHz.fits" % int(1000*freq)
+    if os.path.isfile(filename)==True:
+        gsmMap.from_fits(filename)
+    else:
         g = a.healpix.HealpixMap(nside=512)
         g.map = GlobalSkyModel(freq=1000*freq, GSMlocation=path, GSMNSIDE=512).map # galactic coordinates
         gsmMap.from_hpm(g)
-        return gsmMap
+        gsmMap.to_fits(filename)
+    return gsmMap
 
 def hourAngle(sidereal_time, ra):
     return sidereal_time - ra
@@ -27,7 +32,8 @@ aa = a.cal.get_aa(calfile, freqs)
 aa.set_jultime()
 jd_init = aa.get_jultime()
 print jd_init
-jds = np.linspace(jd_init, jd_init + 0.5, 12)
+jds = np.array([jd_init])
+#jds = np.linspace(jd_init, jd_init + 0.5, 12)
 
 # generate galactic sky
 hpm = makeGSM(nside=64, freq=freqs[0])
@@ -35,12 +41,11 @@ sky = a.healpix.HealpixMap(nside=64)
 
 for i in np.arange(len(jds)):
     aa.set_jultime(jds[i])
-    gxyz = gx,gy,gz, = hpm.px2crd(np.arange(hpm.npix())) # galactic
-    ga2eq = a.coord.convert_m(isys='eq', osys='ga', oepoch=aa.epoch)
-    # conversion matrix so the isys and osys are reversed
-    exyz = ex,ey,ez = np.dot(ga2eq, hpm.px2crd(np.arange(hpm.npix()))) # equatorial
-    txyz = tx,ty,tz = np.dot(a.coord.eq2top_m(aa.sidereal_time(), aa.lat), exyz)
-    az,alt = a.coord.top2azalt(txyz)
-    sky.map = hpm[az,alt]
+    txyz = tx,ty,tz, = sky.px2crd(np.arange(sky.npix())) # topocentric
+    eq2ga = a.coord.convert_m(isys='eq', osys='ga', oepoch=aa.epoch)
+    top2eq = np.linalg.inv(aa.eq2top_m)
+    top2ga = np.dot(eq2ga, top2eq)
+    gxyz = gx,gy,gz = np.dot(top2ga, txyz) # galactic
+    sky.map = hpm[gx,gy,gz]
     uvtools.plot.plot_hmap_ortho(sky, res=1)
     pl.show()
