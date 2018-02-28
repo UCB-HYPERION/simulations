45db4bc47fbf1b59219d6702c6263d52124b8a4b
Kara Kundert <karakundert@gmail.com>
2018-02-27 19:33:54 -0800

    updated unittests for absorber class, all working

--- hypersim/src/tests/absorber_test.py
+++ hypersim/src/tests/absorber_test.py
@@ -23,15 +23,15 @@ class TestAbsorber(unittest.TestCase):
         self.assertTrue(np.all(self.abs.horizon_angle == self.ang))
     def test_abs_response(self):
         xyz = np.array([0,0,1])
-        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, data_file=False, flat=self.dB),1)
+        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, absorber=self.dB),1)
         xyz = np.array([0,1,0])
-        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, data_file=False, flat=self.dB),10**(self.dB/20.))
+        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, absorber=self.dB),10**(self.dB/20.))
         ang1 = self.ang + 0.01
         ang2 = self.ang - 0.01
         xyz = np.array([0,np.cos(ang1),np.sin(ang1)])
-        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, data_file=False, flat=self.dB),1)
+        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, absorber=self.dB),1)
         xyz = np.array([0,np.cos(ang2),np.sin(ang2)])
-        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, data_file=False, flat=self.dB),10**(self.dB/20.))
+        self.assertEqual(self.abs.abs_response(xyz, self.fq, self.smooth, absorber=self.dB),10**(self.dB/20.))
 
 class TestBeamAbsorber(unittest.TestCase):
     def setUp(self):
@@ -49,19 +49,18 @@ class TestBeamAbsorber(unittest.TestCase):
         dB = 0.
         h = a.healpix.HealpixBase(nside=64)
         top = h.px2crd(np.arange(h.npix()))
-        no_abs = self.bm.response(top, self.smooth, data_file=False, flat=dB, use_abs=False)
-        abs_0dB = self.bm.response(top, self.smooth, data_file=False, flat=dB, use_abs=True)
+        no_abs = self.bm.response(top, self.smooth, absorber=dB, use_abs=False)
+        abs_0dB = self.bm.response(top, self.smooth, absorber=dB, use_abs=True)
         np.testing.assert_equal(no_abs, abs_0dB)
         dB = -15.
-        abs_15dB = self.bm.response(top, self.smooth, data_file=False, flat=dB, use_abs=True)
+        abs_15dB = self.bm.response(top, self.smooth, absorber=dB, use_abs=True)
         self.assertFalse(np.all(no_abs == abs_15dB))
         ang1 = self.ang + 0.01
         ang2 = self.ang - 0.01
         xyz = np.array([0,np.cos(ang1),np.sin(ang1)])
-        np.testing.assert_equal(self.bm.response(xyz, self.smooth, data_file=False, flat=dB, use_abs=True),self.bm.beam_response(xyz))
+        np.testing.assert_equal(self.bm.response(xyz, self.smooth, absorber=dB, use_abs=True),self.bm.beam_response(xyz))
         xyz = np.array([0,np.cos(ang2),np.sin(ang2)])
-        np.testing.assert_equal(self.bm.response(xyz, self.smooth, data_file=False, flat=dB, use_abs=True),self.bm.beam_response(xyz)*10**(-15/20.))
-
+        np.testing.assert_equal(self.bm.response(xyz, self.smooth, absorber=dB, use_abs=True),self.bm.beam_response(xyz)*10**(-15/20.))
 
 if __name__ == '__main__':
     unittest.main()
